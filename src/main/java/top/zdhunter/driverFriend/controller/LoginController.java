9d2e9aca54da1f28982211b6469213573f5137bf
usongon <zdhuntero@gmail.com>
2019-11-12 00:54:36 +0800

    管理员登陆所需要Dao层接口

--- src/main/java/top/zdhunter/driverFriend/controller/LoginController.java
+++ src/main/java/top/zdhunter/driverFriend/controller/LoginController.java
@@ -1,8 +1,14 @@
 package top.zdhunter.driverFriend.controller;
 
+import top.zdhunter.driverFriend.bean.param.LoginParams;
+
 /**
  * @author zhangdehua
  * @date 2019-11-11
  */
 public class LoginController {
+
+    Object Login(LoginParams params){
+        return 1;
+    }
 }
